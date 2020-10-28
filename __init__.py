442d1b8c117e1957d168eba016a7627400b20246
adampy <adamflint8@hotmail.co.uk>
2020-10-28 12:45:29 +0000

    Basic API using GCSE DB

---
+++ __init__.py
@@ -0,0 +1,33 @@
+﻿from quart import Quart
+import student
+import asyncpg
+import asyncio
+from csv import reader
+import os
+
+try:
+    with open("credentials.csv", 'r') as f:
+        read = reader(f)
+        for vars in read:
+            os.environ[vars[0]] = vars[1]
+except FileNotFoundError:
+    pass
+
+def create_app():
+    '''This subroutine creates the Quart app and returns it. The DB is also connected in this step.'''
+    app = Quart(__name__)
+    app.register_blueprint(student.bp)
+
+    @app.before_serving
+    async def on_startup():
+        app.config['pool'] = await asyncpg.create_pool(os.environ['GCSE_DATABASE_URL'] + "?sslmode=require", max_size=20)
+
+    return app
+
+def main():
+    '''This subroutine runs at the start of the program.'''
+    app = create_app()
+    app.run(debug = True)
+
+if __name__ == "__main__":
+    main()
---
+++ student.py
@@ -0,0 +1,18 @@
+﻿from quart import Blueprint, request, abort, current_app
+import asyncpg
+
+bp = Blueprint("student", __name__, url_prefix = "/student")
+
+@bp.route('/<id>', methods = ['GET'])
+async def student_function(id):
+    to_return = None
+    async with current_app.config['pool'].acquire() as connection:
+        async with connection.transaction():
+            data = await connection.fetch("SELECT * FROM qotd WHERE id = $1", int(id))
+            to_return = data[0][1]
+
+    return to_return
+
+@bp.route('/error', methods = ['GET'])
+async def error():
+    abort(401)
