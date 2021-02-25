f5d009be4317d94de4cd5e594a7b406160a9932a
Adam Crawley <adamflint8@hotmail.co.uk>
2021-02-25 01:24:40 +0000

    Fixed stupid update bug

--- managers.py
+++ managers.py
@@ -150,8 +150,8 @@ class StudentManager(AbstractUserManager):
         reset_password = False (defaults to False, can be turned to True if the passwords needs resetting)
         new_password = '' (if a new password is given, it will be changed and a new salt is generated."""
         
-        username_exists = await self.db.execute("SELECT * FROM teacher WHERE username = $1 AND id != $2", student.username, current_student.id)
-        if username_exists:
+        query = await self.db.fetchrow("SELECT * FROM student WHERE username = $1 AND id != $2", student.username, current_student.id)
+        if query.get("exists"):
             raise UsernameTaken
         
         self.cache.remove(current_student.username) # Remove from cache
@@ -189,8 +189,8 @@ class TeacherManager(AbstractUserManager):
     async def update(self, current_teacher: Teacher, teacher: Teacher, new_password = ''):
         """Procedure that updates a given teacher. Takes in a current_teacher, updated_teacher and an optional new_password."""
         
-        username_exists = await self.db.execute("SELECT * FROM teacher WHERE username = $1 AND id != $2", teacher.username, current_teacher.id)
-        if username_exists:
+        query = await self.db.fetchrow("SELECT * FROM teacher WHERE username = $1 AND id != $2", teacher.username, current_teacher.id)
+        if query.get("exists"):
             raise UsernameTaken
         
         self.cache.remove(current_teacher.username)
